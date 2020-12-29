7d5ded7959546b6b6114fa963972ddbbef416859
synboorzy <mikolaj.rams2137@gmail.com>
2020-12-29 21:27:56 +0100

    zajebiste zmiany wieksza rozdielczosc

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,28 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(400, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
     sf::CircleShape shape(100.f);
-	sf::Uint8        *pixels  = new sf::Uint8[400 * 200 * 4];
+	sf::Uint8        *pixels  = new sf::Uint8[1280 * 720 * 4];
     shape.setFillColor(sf::Color::Green);
 	
 	sf::Texture texture;
-	texture.create(400, 200); 
+	texture.create(1280, 720); 
 	
 	
 	sf::Sprite sprite(texture);
 	slowvoronoi sv;
-	std::vector<std::pair<int,int>> points= sv.randpoints(20,2137,400,200);
-	std::vector<float> dists = sv.slow(400,200,5,points);
+	std::vector<std::pair<int,int>> points= sv.randpoints(60,2137,1280,720);
+	std::vector<float> dists = sv.slow(1280,720,5,points);
 	
-	for(int x=0;x<400;x++)
+	for(int x=0;x<1280;x++)
 	{
-		for(int y=0;y<200;y++)
+		for(int y=0;y<720;y++)
 		{
-			pixels[4*(y * 400+x)]   = dists[y*400+x]; // R?
-			pixels[4*(y * 400+x)+1] = dists[y*400+x];  // G?
-			pixels[4*(y * 400+x)+2] = dists[y*400+x];  // B?
-			pixels[4*(y * 400+x)+3] = 255; // A?
+			pixels[4*(y * 1280+x)]   = dists[y*1280+x]; // R?
+			pixels[4*(y * 1280+x)+1] = dists[y*1280+x];  // G?
+			pixels[4*(y * 1280+x)+2] = dists[y*1280+x];  // B?
+			pixels[4*(y * 1280+x)+3] = 255; // A?
 		}
 	}
 	
@@ -40,11 +40,11 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+		window.clear();
         window.draw(sprite);
         window.display();
 		/*
-        window.clear();
+        
         window.draw(shape);
         window.display();
 		*/
